500e3782d918392809319648e0895f767ba019ee
Avi Mukesh <avimukesh10@gmail.com>
2022-09-11 19:57:23 +0100

    added header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import AddPostForm from "./features/posts/AddPostForm"
 import PostsList from "./features/posts/PostsList"
 
+import { Routes, Route } from "react-router-dom"
+import SinglePostPage from "./features/posts/SinglePostPage"
+import Layout from "./components/Layout"
+
 function App() {
     return (
-        <main className="App">
-            <AddPostForm />
-            <PostsList />
-        </main>
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<PostsList />} />
+
+                <Route path="post">
+                    <Route index element={<AddPostForm />} />
+                    <Route path=":postId" element={<SinglePostPage />} />
+                </Route>
+            </Route>
+        </Routes>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { store } from "./app/store"
 import { Provider } from "react-redux"
 import { fetchUsers } from "./features/users/usersSlice"
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
 // load the users in as soon as the app loads
 store.dispatch(fetchUsers())
 
@@ -13,7 +15,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
     // <React.StrictMode>
     <Provider store={store}>
-        <App />
+        <Router>
+            <Routes>
+                <Route path="/*" element={<App />} />
+            </Routes>
+        </Router>
     </Provider>
     // </React.StrictMode>
 )
